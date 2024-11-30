97df0cd5cccfcd2bad9daeeb680263bb5ec0ad5b
JavierAndradeGIT <ejavierandradez@gmail.com>
2024-11-30 12:53:00 -0600

    UI cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  get "profiles/show"
   resources :communities
   resources :submissions do
     resources :comments
   end
   devise_for :users
   root "submissions#index"
+
+  resources :profiles, only: :show
 
   get "/pepito", to: "submissions#mostrar"
 end
